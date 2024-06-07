1559d3aaf61da9b97c19de94420d4a262d9ee2d2
shmatozz <baryshev.matvey@yandex.ru>
2024-06-07 15:09:45 +0300

    load base app icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -23,7 +23,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    Inter: require('../assets/fonts/Inter-Regular.ttf'),
     ...FontAwesome.font,
   });
 
