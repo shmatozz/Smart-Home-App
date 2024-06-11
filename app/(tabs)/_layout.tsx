1251a310eaa582099f6a4fc9666fd86df9982d36
shmatozz <baryshev.matvey@yandex.ru>
2024-06-11 15:06:53 +0300

    add icons to tab bar screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,44 @@
 import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable } from 'react-native';
+import { Tabs } from 'expo-router';
 
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
-}
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function TabLayout() {
-    const colorScheme = useColorScheme();
+    // const colorScheme = useColorScheme();
 
     return (
         <Tabs
             screenOptions={{
-                tabBarActiveTintColor: Colors[colorScheme ?? 'light'].blue["50"],
+                tabBarActiveTintColor: Colors.light.blue["50"],
                 headerShown: false,
                 tabBarShowLabel: false
             }}>
-                <Tabs.Screen name="home"/>
-                <Tabs.Screen name="room"/>
-                <Tabs.Screen name="add_device"/>
-                <Tabs.Screen name="security"/>
-                <Tabs.Screen name="stats"/>
+                <Tabs.Screen name="home"
+                             options={{
+                                 tabBarIcon: ({color}) => <MaterialIcons name="home" size={28} color={ color } />
+                             }}
+                />
+                <Tabs.Screen name="room"
+                             options={{
+                                tabBarIcon: ({color}) => <MaterialIcons name="room" size={28} color={ color } />
+                             }}
+                />
+                <Tabs.Screen name="add_device"
+                             options={{
+                                tabBarIcon: ({color}) => <MaterialIcons name="add" size={28} color={ color } />
+                             }}
+                />
+                <Tabs.Screen name="security" options={{
+                                tabBarIcon: ({color}) => <MaterialIcons name="shield" size={28} color={ color } />
+                             }}
+                />
+                <Tabs.Screen name="stats" options={{
+                                tabBarIcon: ({color}) => <MaterialIcons name="bar-chart" size={28} color={ color } />
+                             }}
+                />
         </Tabs>
     );
 }
