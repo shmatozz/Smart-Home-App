e85055d0848a35f96b1068438558a734acf71753
shmatozz <baryshev.matvey@yandex.ru>
2024-06-13 09:50:53 +0300

    add page header component

---
+++ components/visual/PageHeader.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+    Text,
+    View,
+    StyleSheet,
+    Pressable,
+} from 'react-native';
+import Colors from "@/constants/Colors";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+interface HeaderProps {
+    backIcon?: boolean,
+    onBackPress?: () => void,
+    title: string,
+    firstIcon?: keyof typeof MaterialIcons.glyphMap | null,
+    onFirstPress?: () => void,
+    secondIcon?: keyof typeof MaterialIcons.glyphMap | null,
+    onSecondPress?: () => void,
+}
+
+const Header: React.FC<HeaderProps> = ({
+                                           backIcon = false,
+                                           onBackPress = () => console.log("Back pressed"),
+                                           title,
+                                           firstIcon = null,
+                                           onFirstPress = () => console.log("First pressed"),
+                                           secondIcon = null,
+                                           onSecondPress = () => console.log("First pressed"),
+                                       }) => {
+    return (
+        <View style={ styles.container }>
+            { backIcon && <Pressable onPress={ () => onBackPress() }>
+                <MaterialIcons name={"arrow-back"} size={ 28 } color={ Colors.light.base['70'] }/>
+            </Pressable> }
+
+            <MaterialIcons name="account-circle" size={ 32 } color={ Colors.light.base['70'] }/>
+
+            <Text style={ styles.text }>{ title }</Text>
+
+            { firstIcon && <Pressable onPress={ () => onFirstPress() }>
+                <MaterialIcons name={ firstIcon } size={ 28 } color={ Colors.light.base['70'] }/>
+            </Pressable> }
+
+            { secondIcon && <Pressable onPress={ () => onSecondPress() }>
+                <MaterialIcons name={ secondIcon } size={ 28 } color={ Colors.light.base['70'] }/>
+            </Pressable> }
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 56,
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 16,
+        paddingHorizontal: 16,
+    },
+    text: {
+        fontSize: 20,
+        color:  Colors.light.base['90'],
+        flex: 1,
+        fontFamily: "Inter"
+    }
+});
+
+export default Header;
