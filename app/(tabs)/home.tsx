814e0e1e97b1ff57ad5437f09707e996f7cfc23a
shmatozz <baryshev.matvey@yandex.ru>
2024-06-14 11:41:30 +0300

    add rooms and recent devices scroll lists

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,61 +1,99 @@
 import React from "react";
-import {Text, View, StyleSheet, Animated, ScrollView, FlatList} from 'react-native';
+import {Text, View, StyleSheet, FlatList, ScrollView, StatusBar} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from "@/components/visual/PageHeader";
 import Colors from "@/constants/Colors";
 import InfoCard from "@/components/cards/InfoCard";
 import Button from "@/components/buttons/Button";
 import ImageCard from "@/components/cards/ImageCard";
+import {color} from "ansi-fragments";
+import DeviceWideCard from "@/components/cards/DeviceWideCard";
+
+const cardsData = [
+    {
+        image: null,
+        title: 'Title 1',
+        subtitle: 'Subtitle 1',
+    },
+    {
+        image: null,
+        title: 'Title 2',
+        subtitle: 'Subtitle 2',
+    },
+    {
+        image: null,
+        title: 'Title 3',
+        subtitle: 'Subtitle 3',
+    },
+];
+
+const devicesData = [
+    {
+        image: null,
+        title: 'TV',
+        subtitle: 'Living room',
+    },
+    {
+        image: null,
+        title: 'Lamp',
+        subtitle: 'Bedroom',
+    },
+    {
+        image: null,
+        title: 'Aircooler',
+        subtitle: 'Kitchen',
+    },
+]
 
 const Home = () => {
-    const cardsData = [
-        {
-            image: null,
-            title: 'Title 1',
-            subtitle: 'Subtitle 1',
-        },
-        {
-            image: null,
-            title: 'Title 2',
-            subtitle: 'Subtitle 2',
-        },
-        {
-            image: null,
-            title: 'Title 3',
-            subtitle: 'Subtitle 3',
-        },
-    ];
-
     return (
         <SafeAreaView style={ styles.safeArea }>
+            <StatusBar barStyle='dark-content' />
             <Header title={"Welcome, Matvey!"} firstIcon={"notifications"}/>
 
-            <View style={ styles.contentContainer }>
-                <View style={ styles.infoCards }>
-                    <InfoCard icon={ "thermostat" } title={ "Inside temperature" } info={ "26" }/>
-                    <InfoCard icon={ "devices-other" } title={ "Active devices" } info={ "5" }/>
-                    <InfoCard icon={ "bolt" } title={ "Electricity usage" } info={ "36 kWh" }/>
-                </View>
-
-                <View style={ styles.roomsContainer }>
-                    <View style={{ flexDirection: "row", alignItems: "center" }}>
-                        <Text style={ styles.roomsTitle }>{ "Your Rooms" }</Text>
-                        <Button text={ "See all" } type={ "tertiary" } />
+            <ScrollView showsVerticalScrollIndicator={false} overScrollMode={"never"}>
+                <View style={ styles.contentContainer }>
+                    <View style={ styles.infoCards }>
+                        <InfoCard icon={ "thermostat" } title={ "Inside\ntemperature" } info={ "26" }/>
+                        <InfoCard icon={ "devices-other" } title={ "Active\ndevices" } info={ "5" }/>
+                        <InfoCard icon={ "bolt" } title={ "Electricity usage" } info={ "36 kWh" }/>
                     </View>
 
-                    <FlatList
-                        style={ styles.scrollContainer }
-                        horizontal={ true }
-                        data={ cardsData }
-                        renderItem={({item}) => <ImageCard image={ item.image } title={ item.title }/>}
-                        keyExtractor={item => item.title}
-                    />
-                </View>
+                    <View style={ styles.roomsContainer }>
+                        <View style={{ flexDirection: "row", alignItems: "center" }}>
+                            <Text style={ styles.roomsTitle }>{ "Your Rooms" }</Text>
+                            <Button text={ "See all" } type={ "tertiary" } />
+                        </View>
 
-                <View style={ styles.recentDevicesContainer }>
+                        <FlatList
+                            style={{ overflow: 'visible' }}
+                            horizontal={ true }
+                            data={ cardsData }
+                            showsHorizontalScrollIndicator={ false }
+                            overScrollMode={ "never" }
+                            renderItem={({item}) => <ImageCard image={ item.image } title={ item.title } subtitle={ item.subtitle }/>}
+                            ItemSeparatorComponent={() => <View style={{ width: 8 }}/>}
+                            keyExtractor={item => item.title}
+                        />
+                    </View>
 
+                    <View style={ styles.recentDevicesContainer }>
+                        <View style={{ flexDirection: "row", alignItems: "center" }}>
+                            <Text style={ styles.roomsTitle }>{ "Recent devices" }</Text>
+                            <Button text={ "See all" } type={ "tertiary" } />
+                        </View>
+
+                        <View style={{ gap: 12 }}>
+                            { devicesData.map((device, index) => (
+                                <DeviceWideCard key={ index }
+                                                image={ device.image }
+                                                title={ device.title }
+                                                subtitle={ device.subtitle }/>
+                            ))}
+                        </View>
+                    </View>
                 </View>
-            </View>
+            </ScrollView>
         </SafeAreaView>
     );
 }
@@ -77,6 +115,7 @@ const styles = StyleSheet.create({
         gap: 16,
     },
     roomsContainer: {
+        overflow: 'visible',
         gap: 16,
         paddingVertical: 20,
     },
@@ -85,13 +124,8 @@ const styles = StyleSheet.create({
         fontFamily: "Inter",
         fontSize: 20,
     },
-    scrollContainer: {
-        flex: 1,
-        height: 144,
-        gap: 8,
-    },
     recentDevicesContainer: {
-
+        gap: 16,
     }
 });
 
