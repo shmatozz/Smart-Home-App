ae789c6e936a52f0cee5d753c173a305cb14366b
shmatozz <baryshev.matvey@yandex.ru>
2024-06-25 14:16:00 +0300

    update switch (fix pressable areas)

--- components/choice/Switch.tsx
+++ components/choice/Switch.tsx
@@ -2,15 +2,19 @@ import React, {useEffect, useRef, useState} from 'react';
 import {
     Text,
     StyleSheet,
-    Pressable, View, Animated,
+    Pressable, View, Animated, StyleProp, ViewStyle,
 } from 'react-native';
 import Colors from "@/constants/Colors";
 
 interface SwitchProps {
     text?: string,
+    style?: StyleProp<ViewStyle> | null,
 }
 
-const Switch: React.FC<SwitchProps> = ({ text}) => {
+const Switch: React.FC<SwitchProps> = ({
+                                           text,
+                                           style = null,
+                                       }) => {
     const [checked, setChecked] = useState(false);
     const moveAnimation = useRef(new Animated.Value(0)).current;
     useEffect(() => {
@@ -30,7 +34,7 @@ const Switch: React.FC<SwitchProps> = ({ text}) => {
     }, [checked])
 
     return (
-        <Pressable style={ styles.container } onPress={ () => setChecked(!checked) }>
+        <Pressable style={ [styles.container, style] } onPress={ () => setChecked(!checked) }>
             <Text style={ styles.text }>{ text }</Text>
             <View style={ [styles.switchBackground,
                 checked ? {
@@ -52,6 +56,7 @@ const Switch: React.FC<SwitchProps> = ({ text}) => {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
         flexDirection: 'row',
         alignItems: 'center',
         gap: 4,
