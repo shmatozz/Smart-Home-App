b856138887899e806be6ae1bf03e2432da8e99d5
MaffyMaths <matvey33.baryshev@mail.ru>
2024-07-02 15:28:36 +0300

    update drop down select (add 'S' size)

--- components/choice/DropdownSelect.tsx
+++ components/choice/DropdownSelect.tsx
@@ -10,6 +10,7 @@ interface DropdownSelectProps {
     options: string[];
     selectedOption: string;
     onOptionSelected: (option: string) => void;
+    size: 'S' | 'M';
 }
 
 const DropdownSelect: React.FC<DropdownSelectProps> = ({
@@ -18,13 +19,25 @@ const DropdownSelect: React.FC<DropdownSelectProps> = ({
                                                            options,
                                                            selectedOption,
                                                            onOptionSelected,
+                                                           size = 'M'
                                                        }) => {
     const [isOpen, setIsOpen] = useState(false);
     const optionsHeight = useSharedValue(0);
 
+    let styles: { contentContainer: any; optionsContainer: any; option: any; optionText: any; selectedContainer: any; placeholderText: any; selectedText: any; };
+    let optionSize: number;
+
+    if (size == 'S') {
+        styles = stylesS;
+        optionSize = 36;
+    } else {
+        styles = stylesM;
+        optionSize = 48;
+    }
+
     const toggleDropdown = () => {
         setIsOpen(!isOpen);
-        optionsHeight.value = withTiming(isOpen ? 0 : options.length * 48, { duration: 200 });
+        optionsHeight.value = withTiming(isOpen ? 0 : options.length * optionSize, { duration: 200 });
     };
 
     const handleOptionSelect = (option: string) => {
@@ -41,7 +54,7 @@ const DropdownSelect: React.FC<DropdownSelectProps> = ({
 
     return (
         <View style={ styles.contentContainer }>
-            <Animated.View style={ [styles.optionsContainer, animatedStyle, { bottom: 52 }] }>
+            <Animated.View style={ [styles.optionsContainer, animatedStyle] }>
                 { options.map((option, index) => (
                     <Pressable key={ index } onPress={ () => handleOptionSelect(option) } style={ styles.option }>
                         <Text style={ styles.optionText }>{ option }</Text>
@@ -66,7 +79,7 @@ const DropdownSelect: React.FC<DropdownSelectProps> = ({
     );
 };
 
-const styles = StyleSheet.create({
+const stylesM = StyleSheet.create({
     contentContainer: {
         flex: 1,
         borderRadius: 6,
@@ -85,6 +98,7 @@ const styles = StyleSheet.create({
         borderTopLeftRadius: 6,
         borderLeftWidth: 2,
         borderRightWidth: 2,
+        bottom: 51,
     },
     selectedContainer: {
         height: 52,
@@ -115,6 +129,61 @@ const styles = StyleSheet.create({
     },
     optionText: {
         fontSize: 16,
+        fontFamily: 'Inter',
+        color: Colors.light.blue['50'],
+    },
+});
+
+const stylesS = StyleSheet.create({
+    contentContainer: {
+        flex: 1,
+        borderRadius: 6,
+        borderColor: Colors.light.blue["50"],
+        borderWidth: 1,
+    },
+    optionsContainer: {
+        position: 'absolute',
+        overflow: 'hidden',
+        width: '100%',
+        backgroundColor: Colors.light.base["0"],
+        zIndex: 1,
+        borderColor: Colors.light.blue["50"],
+        borderTopWidth: 1,
+        borderTopRightRadius: 6,
+        borderTopLeftRadius: 6,
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        bottom: 38,
+    },
+    selectedContainer: {
+        height: 40,
+        paddingHorizontal: 16,
+        paddingVertical: 8,
+        gap: 8,
+        alignItems: 'center',
+        flexDirection: 'row',
+        backfaceVisibility: 'hidden',
+    },
+    selectedText: {
+        flex: 1,
+        fontSize: 14,
+        fontFamily: 'Inter',
+        color: Colors.light.blue['50'],
+    },
+    placeholderText: {
+        flex: 1,
+        fontSize: 14,
+        fontFamily: 'Inter',
+        fontStyle: 'italic',
+        color: Colors.light.blue['40'],
+    },
+    option: {
+        height: 36,
+        justifyContent: 'center',
+        paddingHorizontal: 16,
+    },
+    optionText: {
+        fontSize: 14,
         fontFamily: 'Inter',
         color: Colors.light.blue['50'],
     },
